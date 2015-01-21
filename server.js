deeacafe663b3ea6305c1f5173de1c723dcd26fc
Jekt <jekt.tl@gmail.com>
2015-01-21 10:47:59 +0100

    Fetch general og tags

--- server.js
+++ server.js
@@ -12,20 +12,69 @@ app.get('/scrape', function(req, res){
 
 	request(url, function(error, response, html){
 		if(!error){
-			var $ = cheerio.load(html);
-
-			var json = { title : "", description : "", thumbnail : ""};
-
-			json.title 		 = $('head meta[property="og:title"]').attr('content');
-			json.description = $('head meta[property="og:description"]').attr('content');
-			json.thumbnail 	 = $('head meta[property="og:image"]').attr('content');
+			var $ 	 = cheerio.load(html),
+				json = JSON.stringify(fetchOGTags($), null, 4);
 		}
 
-		console.log(JSON.stringify(json, null, 4));
-        res.send(JSON.stringify(json, null, 4));
+		console.log(json);
+        res.send(json);
 	})
 })
 
 app.listen('1234', function(){
 	console.log('Listening on port 1234');
 });
+
+function fetchOGTags($){
+	var object = {
+
+		// General
+		'ogTitle' 		: $('head meta[property="og:title"]').attr('content'),
+		'ogDescription' : $('head meta[property="og:description"]').attr('content'),
+		'ogUrl' 		: $('head meta[property="og:url"]').attr('content'),
+		'ogDeterminer' 	: $('head meta[property="og:determiner"]').attr('content'),
+		'ogSiteName' 	: $('head meta[property="og:site_name"]').attr('content'),
+
+		// Type
+		'ogType' 		: $('head meta[property="og:type"]').attr('content'),
+
+		// Locale
+		'ogLocale' 		: {
+			'locale' 	: $('head meta[property="og:locale"]').attr('content'),
+			'localeAlts'	: $('head meta[property="og:locale:alternate"]')
+								.map(function(){return $(this).attr('content')})
+								.get()
+		},
+
+		// Video
+		'ogVideo' 		: {
+			'url' 		: $('head meta[property="og:video"]').attr('content'),
+			'secureUrl' : $('head meta[property="og:video:secure_url"]').attr('content'),
+			'type' 		: $('head meta[property="og:video:type"]').attr('content'),
+			'width' 	: $('head meta[property="og:video:width"]').attr('content'),
+			'height' 	: $('head meta[property="og:video:height"]').attr('content')
+		},
+
+		// Audio
+		'ogAudio' 		: {
+			'url' 		: $('head meta[property="og:audio"]').attr('content'),
+			'secureUrl' : $('head meta[property="og:audio:secure_url"]').attr('content'),
+			'type' 		: $('head meta[property="og:audio:type"]').attr('content')
+		},
+	
+		// Image 
+		'ogImage'		: {
+			'urls'		: ($('head meta[property="og:image"]') || $('head meta[property="og:image:url"]'))
+								.map(function(){return $(this).attr('content')})
+								.get(),
+			'secureUrls': $('head meta[property="og:image:secure_url"]')
+								.map(function(){return $(this).attr('content')})
+								.get(),
+			'type' 		: $('head meta[property="og:image:type"]').attr('content'),
+			'width' 	: $('head meta[property="og:image:width"]').attr('content'),
+			'height' 	: $('head meta[property="og:image:height"]').attr('content')
+		}
+	};
+
+	return object;
+}
