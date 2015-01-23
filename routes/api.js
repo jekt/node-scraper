a290161ad8d778dae9fc125271b0c8305f395182
Jekt <jekt.tl@gmail.com>
2015-01-23 21:55:23 +0100

    Remove unused vars from api routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,6 @@
 'use strict';
 
-var express = require('express'),
-	app     = express(),
-	scraper = require('../lib/scraper');
+var scraper = require('../lib/scraper');
 
 module.exports = function (app) {
 	app.get('/fetch', scraper.fetch);
